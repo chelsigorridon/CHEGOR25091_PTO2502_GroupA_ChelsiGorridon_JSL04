ff169cab09ad81f5ae91caec4249efa5c3a844ba
chelsigorridon <chelsijaimegorridon@gmail.com>
2025-07-19 11:20:02 +0200

    Added a Button and event Listener

--- Scripts/script.js
+++ Scripts/script.js
@@ -1,3 +1,7 @@
 import { initialTasks } from "../Data/initialData";
 
+const cardColumn = document.getElementsByClassName("card-column-main");
+console.log(cardColumn)
 
+const button = document.getElementById("add-task-button");
+button.addEventListener("click",()=>{})
