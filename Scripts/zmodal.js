8b4fccaaad4e222855689b620e1c28d1451faceb
chelsigorridon <chelsijaimegorridon@gmail.com>
2025-07-19 20:24:10 +0200

    Added Export to modal Function

--- Scripts/zmodal.js
+++ Scripts/zmodal.js
@@ -1,16 +1,18 @@
-function openModal(task) {
+
+  export function openModal(task) {  
   document.getElementById("modal-title").innerText = task.title;
   document.getElementById("modal-description").innerText = task.description;
-  
-  
-  const statusSelect = document.getElementById("modal-status");
-  if (statusSelect) {
-    statusSelect.value = task.status.toLowerCase();
+
+    const statusSelect = document.getElementById("modal-status");
+    if (statusSelect) {
+      statusSelect.value = task.status.toLowerCase();
+    }
+
   }
 
   
   const modal = document.getElementById("task-modal");
   if (modal) {
     modal.style.display = "block";
-  }
-}
+  };
+;
